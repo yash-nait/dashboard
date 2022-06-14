27707a345fa5a01bc8fbf6f8334b048644ece6a3
yash-nait <yashnait@gmail.com>
2022-06-14 13:58:50 +0530

    firebase auth error

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,6 +1,8 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 
+console.log(process.env.REACT_APP_FIREBASE_API_KEY)
+
 const app = firebase.initializeApp({
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
